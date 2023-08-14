452abd5b3e8e34ce6c9eeec7f52a5b61295c98b7
Anurag Sidhu <anuragsidhu72@gmail.com>
2023-08-14 11:31:17 +0530

    contract done

--- hardhat/contracts/Lock.sol
+++ hardhat/contracts/Lock.sol
@@ -1,34 +1,78 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    function mint(uint256 amount) public payable {
+        _mint(msg.sender, amount);
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    struct asset {
+        string name;
+        string description;
+        uint price;
+        uint borrowingPrice;
+        address owner;
+        bool buyed;
+        bool borrowed;
+        string image;
+        address Borrowed;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    mapping(uint256 => asset) public allAsset;
+    mapping(address => asset) public addressAsset;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    function createAsset(
+        uint id,
+        string memory _name,
+        string memory _description,
+        uint _price,
+        uint _borrowingPrice,
+        string memory _image
+    ) public {
+        require(balanceOf(msg.sender) > 0, "Don't have sufficient token");
+        allAsset[id].name = _name;
+        allAsset[id].description = _description;
+        allAsset[id].price = _price;
+        allAsset[id].borrowingPrice = _borrowingPrice;
+        allAsset[id].owner = msg.sender;
+        allAsset[id].image = _image;
 
-        owner.transfer(address(this).balance);
+        addressAsset[msg.sender].name = _name;
+        addressAsset[msg.sender].description = _description;
+        addressAsset[msg.sender].price = _price;
+        addressAsset[msg.sender].borrowingPrice = _borrowingPrice;
+        addressAsset[msg.sender].owner = msg.sender;
+        addressAsset[msg.sender].image = _image;
+    }
+
+    function BuyAsset(uint id) public {
+        require(
+            balanceOf(msg.sender) >= allAsset[id].price,
+            "Don't have sufficient token"
+        );
+        transfer(allAsset[id].owner, allAsset[id].price);
+        allAsset[id].owner = msg.sender;
+        allAsset[id].buyed = true;
+        addressAsset[msg.sender].buyed = true;
+    }
+
+    function BorrowAsset(uint id) public {
+        require(
+            balanceOf(msg.sender) >= allAsset[id].borrowingPrice,
+            "Don't have sufficient token"
+        );
+        allAsset[id].borrowed = true;
+        addressAsset[msg.sender].Borrowed = msg.sender;
+        allAsset[id].Borrowed = msg.sender;
+    }
+
+    function returnAsset(uint id, uint amount) public {
+        transfer(allAsset[id].owner, amount);
+        allAsset[id].borrowed = false;
     }
 }
